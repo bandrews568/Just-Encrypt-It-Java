10e1c781e82ae7a74624bf6db9b3534150107848
Brandon Andrews <bandrews568@gmail.com>
2017-02-02 14:23:41 -0500

    refactor

--- app/src/main/java/us/brandonandrews/justencryptit/SettingsActivity.java
+++ app/src/main/java/us/brandonandrews/justencryptit/SettingsActivity.java
@@ -8,18 +8,24 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
 
+
 public class SettingsActivity extends AppCompatActivity {
 
-    private SharedPreferences mPrefs;
-    private SharedPreferences.Editor mEditior;
+    private SharedPreferences prefs;
+    public static EditText[] customPasswordNameEditText;
+    public static EditText[] customPasswordEditText;
+    public static String[] customPasswordNames = new String[] {"customPasswordName1",
+                                                               "customPasswordName2",
+                                                               "customPasswordName3"};
+    public static String[] customPassword = new String[] {"customPassword1",
+                                                         "customPassword2",
+                                                         "customPassword3"};
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        mPrefs = getSharedPreferences("password", Context.MODE_PRIVATE);
-
         EditText etCustomPasswordName1 = (EditText) findViewById(R.id.etCustomPasswordName1);
         EditText etCustomPasswordName2 = (EditText) findViewById(R.id.etCustomPasswordName2);
         EditText etCustomPasswordName3 = (EditText) findViewById(R.id.etCustomPasswordName3);
@@ -28,44 +34,44 @@ public class SettingsActivity extends AppCompatActivity {
         EditText etCustomPassword2 = (EditText) findViewById(R.id.etCustomPassword2);
         EditText etCustomPassword3 = (EditText) findViewById(R.id.etCustomPassword3);
 
-        String getCustomPasswordName1 = mPrefs.getString("customPasswordName1", "");
-        String getCustomPasswordName2 = mPrefs.getString("customPasswordName2", "");
-        String getCustomPasswordName3 = mPrefs.getString("customPasswordName3", "");
+        customPasswordNameEditText = new EditText[] {etCustomPasswordName1,
+                                                     etCustomPasswordName2,
+                                                     etCustomPasswordName3};
+        customPasswordEditText = new EditText[] {etCustomPassword1,
+                                                 etCustomPassword2,
+                                                 etCustomPassword3};
 
-        String getCustomPassword1 = mPrefs.getString("customPassword1", "");
-        String getCustomPassword2 = mPrefs.getString("customPassword2", "");
-        String getCustomPassword3 = mPrefs.getString("customPassword3", "");
+        prefs = getSharedPreferences("password", Context.MODE_PRIVATE);
 
-        etCustomPasswordName1.setText(getCustomPasswordName1);
-        etCustomPasswordName2.setText(getCustomPasswordName2);
-        etCustomPasswordName3.setText(getCustomPasswordName3);
+        for (int i = 0; i < 3; i++) {
+            String getCustomPasswordName = prefs.getString(customPasswordNames[i], "");
+            String getCustomPassword = prefs.getString(customPassword[i], "");
+            customPasswordNameEditText[i].setText(getCustomPasswordName);
+            customPasswordEditText[i].setText(getCustomPassword);
 
-        etCustomPassword1.setText(getCustomPassword1);
-        etCustomPassword2.setText(getCustomPassword2);
-        etCustomPassword3.setText(getCustomPassword3);
+        }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        mEditior = mPrefs.edit();
+        setPasswordSettings();
+    }
 
-        EditText etCustomPasswordName1 = (EditText) findViewById(R.id.etCustomPasswordName1);
-        EditText etCustomPasswordName2 = (EditText) findViewById(R.id.etCustomPasswordName2);
-        EditText etCustomPasswordName3 = (EditText) findViewById(R.id.etCustomPasswordName3);
+    private void setPasswordSettings() {
+        SharedPreferences.Editor editor = prefs.edit();
 
-        EditText etCustomPassword1 = (EditText) findViewById(R.id.etCustomPassword1);
-        EditText etCustomPassword2 = (EditText) findViewById(R.id.etCustomPassword2);
-        EditText etCustomPassword3 = (EditText) findViewById(R.id.etCustomPassword3);
+        for (int i = 0; i < 3; i++) {
+            String currentCustomPasswordName = customPasswordNames[i];
+            String currentCustomPassword = customPassword[i];
+            EditText currentCustomPasswordNameEt = customPasswordNameEditText[i];
+            EditText currentCustomPasswordEt = customPasswordEditText[i];
+            String currentCustomPasswordNameText = currentCustomPasswordNameEt.getText().toString();
+            String currentCustomPasswordText = currentCustomPasswordEt.getText().toString();
 
-        mEditior.putString("customPasswordName1", etCustomPasswordName1.getText().toString());
-        mEditior.putString("customPasswordName2", etCustomPasswordName2.getText().toString());
-        mEditior.putString("customPasswordName3", etCustomPasswordName3.getText().toString());
-
-        mEditior.putString("customPassword1", etCustomPassword1.getText().toString());
-        mEditior.putString("customPassword2", etCustomPassword2.getText().toString());
-        mEditior.putString("customPassword3", etCustomPassword3.getText().toString());
-
-        mEditior.commit();
+            editor.putString(currentCustomPasswordName, currentCustomPasswordNameText);
+            editor.putString(currentCustomPassword, currentCustomPasswordText);
+        }
+        editor.commit();
     }
 }
