317d037152289b1678157402718bdf7ad2af6094
Brandon Andrews <bandrews568@gmail.com>
2017-02-06 12:35:32 -0500

    added spinner menu for custom passwords

--- app/src/main/java/us/brandonandrews/justencryptit/MainActivity.java
+++ app/src/main/java/us/brandonandrews/justencryptit/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -14,16 +15,26 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.jasypt.exceptions.EncryptionOperationNotPossibleException;
+
+import java.util.ArrayList;
+
+import static us.brandonandrews.justencryptit.SettingsActivity.CUSTOM_PASSWORD_MAX;
+import static us.brandonandrews.justencryptit.SettingsActivity.customPassword;
+import static us.brandonandrews.justencryptit.SettingsActivity.customPasswordNames;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,20 +47,75 @@ public class MainActivity extends AppCompatActivity {
 
          final ClipboardManager clipboard = (ClipboardManager)
                  getSystemService(Context.CLIPBOARD_SERVICE);
+
+        final SharedPreferences sharedpreferences = getSharedPreferences("password", Context.MODE_PRIVATE);
+        final SharedPreferences.Editor editor = sharedpreferences.edit();
+        final String getPassword = sharedpreferences.getString("password", "none");
+        boolean savePasswordChecked = sharedpreferences.getBoolean("checked", false);
+
         final EditText etPassword = (EditText) findViewById(R.id.etPassword);
         final EditText etEnterText = (EditText) findViewById(R.id.etEnterText);
         final TextView tvFinalText = (TextView) findViewById(R.id.tvFinalText);
-        final CheckBox cbSavePassword = (CheckBox) findViewById(R.id.cbSavePassword);
+
         final Button btnMakeText = (Button) findViewById(R.id.btnMakeText);
         Button btnClear = (Button) findViewById(R.id.btnClear);
         Button btnCopy = (Button) findViewById(R.id.btnCopy);
         Button btnPaste = (Button) findViewById(R.id.btnPaste);
+
+        final CheckBox cbSavePassword = (CheckBox) findViewById(R.id.cbSavePassword);
         RadioGroup rbEncryptOrDecrypt = (RadioGroup) findViewById(R.id.rgEncryptOrDecrypt);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        SharedPreferences sharedpreferences = getSharedPreferences("password", Context.MODE_PRIVATE);
-        final SharedPreferences.Editor editor = sharedpreferences.edit();
-        final String getPassword = sharedpreferences.getString("password", "none");
-        boolean savePasswordChecked = sharedpreferences.getBoolean("checked", false);
+
+        Spinner spinnerCustomPassword = (Spinner) findViewById(R.id.spinnerCustomPasswords);
+        ArrayList<String> spinnerOptions = new ArrayList<>();
+        spinnerOptions.add("Saved Passwords");
+        spinnerCustomPassword.setVisibility(View.INVISIBLE);
+
+        // Spinner adapter
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, spinnerOptions) {
+            // Used to disable `Saved Passwords`
+            @Override
+            public boolean isEnabled(int position){
+                if(position == 0) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+
+            @Override
+            public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                if (position==0) {
+                    tv.setTextColor(Color.GRAY);
+                }
+                return view;
+            }
+        };
+
+        setSpinnerOptions(sharedpreferences, spinnerOptions, spinnerCustomPassword);
+
+        // Spinner
+        spinnerCustomPassword.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position > 0) {
+                    String setPasswordEditText = sharedpreferences.getString(customPassword[position - 1], "");
+                    etPassword.setText(setPasswordEditText);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Do nothing
+            }
+        });
+
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCustomPassword.setAdapter(spinnerAdapter);
+
 
         if (savePasswordChecked && !getPassword.equals("none")) {
             etPassword.setText(getPassword);
@@ -218,6 +284,20 @@ public class MainActivity extends AppCompatActivity {
     public void makeToast(String msg) {
         Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
     }
+
+    public void setSpinnerOptions(SharedPreferences prefs,
+                                  ArrayList<String> options, Spinner spinnerWidget) {
+        for (int i = 0; i < CUSTOM_PASSWORD_MAX; i++) {
+            String getCustomPasswordName = prefs.getString(customPasswordNames[i], "");
+            String getCustomPassword = prefs.getString(customPassword[i], "");
+
+            if (!getCustomPasswordName.equals("") && !getCustomPassword.equals("")) {
+                options.add(getCustomPasswordName);
+                spinnerWidget.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
 
     private class AsyncCreateText extends AsyncTask<String, Void, String> {
 
